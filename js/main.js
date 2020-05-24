9a81dca42e0e27096a8f9e20d06a02c8aafb1cf0
Mari <m_maklakova@fanshaweonline.ca>
2020-05-23 20:04:46 -1100

    fixing

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
-// this is an IIFE (Immediately Invoked Function Expression)
-// also called the Module pattern
-//
-(() => { // shorthand function declaration (also called a fat arrow function)
-  console.log('fired!');
+(() => { 
+  console.log('click on icons to log id to console');
 
-  // select elements here
-  // querySelectorAll is a one-to-many selection -> find EVERY element that matches the selector
-  let wilds = document.querySelectorAll(".wild"),
-      // one to one connection -> one variable name means one element
-      svgWild = document.querySelector("svg");
+ 
+  let theWilds = document.querySelectorAll('.wild');
 
-        function logID() 
-        { console.log("clicked on a graphic - this one: ", this.id);
-    this.classList.add("selected"); }
+  function logID() {
+    console.log("clicked on a graphic - this one: ", this.id);
+    this.classList.add("selected");
+  }
 
-  theWilds.forEach(wild => wild.addEventListener("click", logID));
-
-  svgWild.addEventListener("click", logID);
+  
+  theWilds.forEach(wild => { wild.addEventListener("click", logID);});
 })();
+
