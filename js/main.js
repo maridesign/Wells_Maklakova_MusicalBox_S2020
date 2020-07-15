61d1e9ce81ac7426a3ffc2fefbe0c8a24d905ff3
Selena Wells <selena.elise.wells@gmail.com>
2020-07-14 21:16:05 -0400

    changes to css and js added to create drag and drop functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,54 @@
-(() => { 
-  console.log('click on icons to log id to console');
+(() => {
+    // set up the puzzle pieces and boards
+    const animals = document.querySelectorAll('.animal'),
+        dropZones = document.querySelectorAll('.drop-zone');
 
- let theWilds = document.querySelectorAll('.wild');
 
-  function logID() {
-    console.log("clicked on a graphic - this one: ", this.id);
-    this.classList.add("selected");
-  }
-  
-  theWilds.forEach(wild => { wild.addEventListener("click", logID);});
+    function allowDrag(event) {
+        console.log('started dragging an animal: this one - ', event.target.id);
+
+        event.dataTransfer.setData('draggedImg', this.id);
+    }
+
+    function allowDragOver(event) {
+        event.preventDefault();
+        console.log('dragged something over me');
+
+    }
+
+    function allowDrop(event) {
+        console.log('dropped something on me');
+        let droppedImage = event.dataTransfer.getData('draggedImg');
+        console.log(event.dataTransfer)
+            //if the length of the number of children is 0 append child 
+        if (event.currentTarget.children.length === 0) {
+            event.target.appendChild(document.querySelector(`#${droppedImage}`));
+        }
+    }
+
+    for (let zone of dropZones) {
+        zone.addEventListener('dragover', allowDragOver);
+        zone.addEventListener('drop', allowDrop);
+    }
+
+
+    animals.forEach(animal => animal.addEventListener('dragstart', allowDrag));
+
+
+
+
+
+    // console.log('click on icons to log id to console');
+
+    // let theWilds = document.querySelectorAll('.wild');
+
+    // function logID() {
+    //     console.log("clicked on a graphic - this one: ", this.id);
+    //     this.classList.add("selected");
+    // }
+
+    // theWilds.forEach(wild => { wild.addEventListener("click", logID); });
+
+
+
 })();
